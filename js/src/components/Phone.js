cbc1d462156dbd5426228492cfe09fd5d4994769
geberele <geberele@gmail.com>
2018-06-06 20:36:51 +0100

    CYI: better logic

--- js/src/components/Phone.js
+++ js/src/components/Phone.js
@@ -4,76 +4,169 @@ import _ from 'lodash'
 
 class Phone extends React.Component {
 
+  /**
+   * Implements constructor().
+   */
   constructor (props) {
     super(props)
 
     this.state = {
-      data: {},
-      colorSelected: ''
+      groupName: '',
+      rating: '',
+      listPhones: {},
+      phoneSelected: {},
+      colourSelected: '',
+      memorySelected: '',
     }
 
-    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleColourChange = this.handleColourChange.bind(this);
+    this.handleCapacityChange = this.handleCapacityChange.bind(this);
     this.getColours = this.getColours.bind(this);
+    this.getCapacities = this.getCapacities.bind(this);
+    this.changePhone = this.changePhone.bind(this);
   }
 
+  /**
+   * Implements componentDidMount().
+   */
   componentDidMount() {
     var self = this
 
     axios.get('https://raw.githubusercontent.com/geberele/choose-your-iphone/master/data/phones.json')
       .then(function (response) {
-        self.setState({data: response.data[0]})
+        self.setState({
+          groupName: response.data[0].groupName,
+          rating: response.data[0].rating,
+          listPhones: response.data[0].deviceSummary,
+          phoneSelected: (!_.isEmpty(response.data[0].deviceSummary)) ? response.data[0].deviceSummary[0] : {},
+          colourSelected: (!_.isEmpty(response.data[0].deviceSummary)) ? response.data[0].deviceSummary[0].colourName : '',
+          memorySelected: (!_.isEmpty(response.data[0].deviceSummary)) ? response.data[0].deviceSummary[0].memory : ''
+        })
       })
       .catch(function (error) {
         console.log(error)
       })
   }
 
-  handleOptionChange(e) {
+  /**
+   * Handles the changes of the radio button for the capacities.
+   */
+  handleCapacityChange(e) {
+    this.changePhone('', e.target.value)
+  }
+
+  /**
+   * Handles the changes of the radio button for the colours.
+   */
+  handleColourChange(e) {
+    this.changePhone(e.target.value)
+  }
+
+  /**
+   * Implements componentDidMount().
+   */
+  changePhone(colour = '', memory = '') {
+    colour = (!_.isEmpty(colour)) ? colour : this.state.colourSelected
+    memory = (!_.isEmpty(memory)) ? memory : this.state.memorySelected
+
+    let filteredByColour = _.filter(this.state.listPhones, (o) => (o.colourName == colour)),
+      filteredByCapacity = _.filter(filteredByColour, (o) => (o.memory == memory))
+
     this.setState({
-      colorSelected: e.target.value
+      phoneSelected: (!_.isEmpty(filteredByCapacity)) ? filteredByCapacity[0] : {},
+      colourSelected: colour,
+      memorySelected: memory
     });
   }
 
-  getColours(list) {
-    let uniqList = _.uniqBy(list, 'colourHex')
+  /**
+   * Get the list of the different colours.
+   */
+  getColours() {
+    let uniqList = _.uniqBy(this.state.listPhones, 'colourHex')
     return _.map(uniqList, (o) => (_.pick(o, ['colourHex', 'colourName'])))
   }
 
+  /**
+   * Get the list of the different capacities.
+   */
+  getCapacities() {
+    let uniqList = _.uniqBy(this.state.listPhones, 'memory')
+    return _.map(uniqList, (o) => (_.pick(o, ['memory'])))
+  }
+
+  /**
+   * Implements render().
+   */
   render() {
-    const {data} = this.state
-    console.log(data)
-    var phoneSelected = (!_.isEmpty(data)) ? JSON.stringify(data.deviceSummary[0], null, 2) : '',
-      colours = this.getColours(data.deviceSummary),
-      options = []
+    const {groupName, rating, phoneSelected, colourSelected, memorySelected} = this.state
+    console.log(phoneSelected)
+
+    var colours = this.getColours(),
+      capacities = this.getCapacities(),
+      radioColours = [], radioCapacities = []
 
     _.each(colours, (colour, index) => {
-      options[index] = (
+      radioColours[index] = (
         <label key={index}>
           <input type="radio" id="contactChoice1"
-                 name="color" value={colour.colourHex}
-                 onChange={this.handleOptionChange} />
+                 name="colour" value={colour.colourName}
+                 checked={colourSelected === colour.colourName}
+                 onChange={this.handleColourChange} />
           {colour.colourName}
         </label>
       )
     })
-    console.log(options)
 
-    if (_.isEmpty(data)) {
+    _.each(capacities, (capacity, index) => {
+      radioCapacities[index] = (
+        <label key={index}>
+          <input type="radio" id="contactChoice133"
+                 name="capacity" value={capacity.memory}
+                 checked={memorySelected === capacity.memory}
+                 onChange={this.handleCapacityChange} />
+          {capacity.memory}
+        </label>
+      )
+    })
+
+    if (_.isEmpty(groupName)) {
       return (
         <div className="phone">
           Loading...
         </div>
       )
+    } else if (_.isEmpty(phoneSelected)) {
+      return (
+        <div className="phone">
+          No phones
+        </div>
+      )
     } else {
       return (
         <div className="phone">
-          <div className="phone__image">{data.groupName}</div>
-          <div className="phone__group-name">{data.groupName}</div>
-          <div className="phone__rating">{data.rating}</div>
-          <div className="phone__desc"><pre>{phoneSelected}</pre></div>
+          <div className="phone__image">
+            <img src={phoneSelected.merchandisingMedia[0]['value']} />
+          </div>
+          <div className="phone__group-name">{groupName}</div>
+          <div className="phone__rating">{rating}</div>
+          <div className="phone__desc">{phoneSelected.displayDescription}</div>
           <form>
-            {options}
+            <div>
+              Colour: {colourSelected}
+              {radioColours}
+            </div>
+            <div>
+              Capacity: {memorySelected}
+              {radioCapacities}
+            </div>
           </form>
+          <div>
+            from £ {phoneSelected.priceInfo.hardwarePrice.oneOffPrice.gross} upfront cost
+          </div>
+          <div>
+            When you pay £ {phoneSelected.priceInfo.bundlePrice.monthlyPrice.gross} a month
+          </div>
         </div>
       )
     }
